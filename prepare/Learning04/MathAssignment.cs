15870513f42ca43c44cec1642877d9a8845031aa
yugidallin <dallin.cabaliza@gmail.com>
2024-07-03 09:52:33 +0800

    inheritance learning activity

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Dynamic;
+
+public class MathAssignment : Assignment
+{
+    private string _textBookSection = "";
+    private string _problems = "";
+
+    public MathAssignment(string studentName, string topic, string textBookSection, string problems) : base(studentName,topic)
+    {
+        _textBookSection = textBookSection;
+        _problems = problems;
+    }
+
+   public string GetHomeworkList()
+   {
+        return $"Section {_textBookSection} Problems {_problems}";
+   } 
+
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assign1 = new Assignment("dallin", "csharp");
+        Console.WriteLine(assign1.GetSummary());
+
+        MathAssignment assign2 = new MathAssignment("dallin", "csharp","7.3", "8-9");
+        Console.WriteLine(assign2.GetSummary());
+        Console.WriteLine(assign2.GetHomeworkList());
+
+        WritingAssignment assign3 = new WritingAssignment("dallin", "csharp", "BOM");
+        Console.WriteLine(assign3.GetSummary());
+        Console.WriteLine(assign3.GetWritingInformation());
     }
 }
