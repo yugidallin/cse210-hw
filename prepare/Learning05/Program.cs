170d2a5b50f29f35dc15309cbc9893ac60fdc2cc
yugidallin <dallin.cabaliza@gmail.com>
2024-07-10 10:08:53 +0800

    Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Red", 5);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Blue", 4, 5);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Green", 6);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,27 @@
+using System.Drawing;
+
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+
+}
